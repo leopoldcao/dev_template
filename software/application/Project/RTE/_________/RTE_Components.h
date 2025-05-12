5cc6b4b2b97987fd932d02365624f2f9f12f8e31
Leopold <rondo_orz@163.com>
2025-05-12 17:58:59 +0800

    init

---
+++ software/application/Project/RTE/_________/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Fire_H759IM' 
+ * Target:  '¹¤³ÌÄ£°å' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32h7xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
